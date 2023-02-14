5b22a4140ad1cc90569fa0164e07a634c00508df
orzehhowski <filmyminecraftnayt@gmail.com>
2023-02-14 18:31:22 +0100

    initial db seed organized

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import bodyParser from "body-parser";
 
 import familyRoutes from "./routes/family";
 import spiderRoutes from "./routes/spider";
-import db from "./util/db";
+import initalSeed from "./seed/initalSeed";
 import Family from "./models/family";
 import Spider from "./models/spider";
 import Image from "./models/image";
@@ -93,56 +93,9 @@ Spider.hasMany(Image, {
 
 // RUNNING APP
 
-db.sync()
-  // db.sync({ force: true })
-  .then(async () => {
-    // await Family.create({
-    //   name: "krzyżakowate",
-    //   latinName: "araneidae",
-    //   appearanceDesc:
-    //     "majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krpie majom <br/>krzyz upie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na majomrzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz dupie",
-    //   behaviorDesc: "robią dwuwymiarowe sieci",
-    //   image: "/img/krzyzak.jpg",
-    //   imageAuthor: "Bartosz Orzechowski",
-    //   resources:
-    //     "https://pl.wikipedia.org/wiki/Krzy%C5%BCakowate https://arages.de/files/checklist2004_araneae.pdf",
-    // }).then((family) => {
-    //   family
-    //     .createSpider({
-    //       name: "krzyzak ogrodowy",
-    //       latinName: "araneidae ogrodae",
-    //       appearanceDesc: "ladny jest",
-    //       behaviorDesc: "sieci plecie",
-    //       resources: "https://pl.wikipedia.org/wiki/Krzy%C5%BCakowate",
-    //     })
-    //     .then((spider) => {
-    //       spider
-    //         .createImage({
-    //           src: "/img/krzyzak.jpg",
-    //           author: "Bartosz Orzechowski",
-    //         })
-    //         .then(() => {
-    //           Family.create({
-    //             name: "kwadratnikowate",
-    //             latinName: "Tetragnathidae",
-    //             image: "/img/pajak1.jpg",
-    //             imageAuthor: "Bartosz Orzechowski",
-    //           }).then((family) => {
-    //             family
-    //               .createSpider({
-    //                 latinName: "Metellina segmentata",
-    //               })
-    //               .then((spider) => {
-    //                 spider.createImage({
-    //                   src: "/img/pajak1.jpg",
-    //                   author: "Bartosz Orzechowski",
-    //                 });
-    //               });
-    //           });
+initalSeed()
+  .then(() => {
     app.listen(8080);
-    // });
-    //       });
-    //   });
   })
   .catch((err) => {
     console.log(err);
---
+++ src/seed/initalSeed.ts
@@ -0,0 +1,50 @@
+import Family from "../models/family";
+import db from "../util/db";
+
+export default async () => {
+  return db.sync({ force: true }).then(async () => {
+    const firstFamily = await Family.create({
+      name: "krzyżakowate",
+      latinName: "araneidae",
+      appearanceDesc:
+        "majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krpie majom <br/>krzyz upie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na majomrzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz na dupie majom krzyz dupie",
+      behaviorDesc: "robią dwuwymiarowe sieci",
+      image: "/img/krzyzak.jpg",
+      imageAuthor: "Bartosz Orzechowski",
+      resources:
+        "https://pl.wikipedia.org/wiki/Krzy%C5%BCakowate https://arages.de/files/checklist2004_araneae.pdf",
+    });
+
+    const firstSpider = await firstFamily.createSpider({
+      name: "krzyzak ogrodowy",
+      latinName: "araneidae ogrodae",
+      appearanceDesc: "ladny jest",
+      behaviorDesc: "sieci plecie",
+      resources: "https://pl.wikipedia.org/wiki/Krzy%C5%BCakowate",
+    });
+
+    await firstSpider.createImage({
+      src: "img/krzyzak.jpg",
+      author: "Bartosz Orzechowski",
+    });
+    await firstSpider.createImage({
+      src: "img/krzyzak2.jpg",
+      author: "Bartosz Orzechowski",
+    });
+
+    const secondFamily = await Family.create({
+      name: "kwadratnikowate",
+      latinName: "Tetragnathidae",
+      image: "/img/pajak1.jpg",
+      imageAuthor: "Bartosz Orzechowski",
+    });
+    const secondSpider = await secondFamily.createSpider({
+      latinName: "Metellina segmentata",
+    });
+
+    await secondSpider.createImage({
+      src: "/img/pajak1.jpg",
+      author: "Bartosz Orzechowski",
+    });
+  });
+};
