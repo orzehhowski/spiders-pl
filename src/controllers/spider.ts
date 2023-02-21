d1adb21c02c2d8ab0f2d52472290997468f57a8e
orzehhowski <filmyminecraftnayt@gmail.com>
2023-02-21 12:56:30 +0100

    spider controller restored

--- src/controllers/spider.ts
+++ src/controllers/spider.ts
@@ -1,39 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import HttpError from "../errors/HttpError";
-import fs from "fs";
-import path from "path";
 
+import HttpError from "../errors/HttpError";
+import unlinkImg from "../util/unlinkImg";
 import Spider from "../models/spider";
 import Family from "../models/family";
 import Image from "../models/image";
 
-interface SpiderInfo {
-  name: string | null;
-  latinName: string;
-  appearanceDesc: string | null;
-  behaviorDesc: string | null;
-  resources: string | null;
-}
-
 class SpiderController {
-  getById(req: Request, res: Response, next: NextFunction) {
-    Spider.findByPk(+req.params.id, {
-      include: Spider.associations.images,
-    })
-      .then((spider) => {
-        if (!spider) {
-          throw new HttpError(404, "Spider not found");
-        }
-        res.status(200).json(spider);
-      })
-      .catch((err) => {
-        next(err);
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const spider = await Spider.findByPk(+req.params.id, {
+        include: Spider.associations.images,
       });
+      if (!spider) {
+        throw new HttpError(404, "Spider not found");
+      }
+      res.status(200).json(spider);
+    } catch (err) {
+      next(err);
+    }
   }
 
-  post(req: Request, res: Response, next: NextFunction) {
+  async post(req: Request, res: Response, next: NextFunction) {
     // add validation
 
+    // restore resources to string
     let resourcesStr: string | null;
     if (typeof req.body.resources === "string") {
       resourcesStr = req.body.resources;
@@ -48,137 +39,109 @@ class SpiderController {
 
     const familyId = +req.body.familyId;
 
-    const spiderInfo: SpiderInfo = {
-      name: req.body.name || null,
-      latinName: req.body.latinName,
-      appearanceDesc: req.body.appearanceDesc || null,
-      behaviorDesc: req.body.behaviorDesc || null,
-      resources: resourcesStr,
-    };
-    const src = req.file?.path.replace("src/public/", "") || "";
+    try {
+      if (!req.file) {
+        throw new HttpError(422, "Image is required");
+      }
 
-    const imageInfo = {
-      src,
-      author: req.body.imageAuthor,
-    };
-    if (imageInfo.src) {
-      Family.findByPk(familyId)
-        .then((family) => {
-          if (family) {
-            return family.createSpider(spiderInfo);
-          } else {
-            throw new HttpError(404, "family not found");
-          }
-        })
-        .then((spider) => {
-          return spider.createImage(imageInfo);
-        })
-        .then(() => {
-          res.status(201).json({
-            message: "Spider created",
-            spider: { ...spiderInfo, image: imageInfo },
-          });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    } else {
-      next(new HttpError(422, "Image is required"));
+      const src = req.file.path.replace("src/public/", "") || "";
+
+      const imageInfo = {
+        src,
+        author: req.body.imageAuthor,
+      };
+      const family = await Family.findByPk(familyId);
+      if (!family) {
+        throw new HttpError(404, "family not found");
+      }
+      const spider = await family.createSpider({
+        ...req.body,
+        resources: resourcesStr,
+      });
+
+      await spider.createImage(imageInfo);
+
+      res.status(201).json({
+        message: "Spider created",
+        spider: { ...spider.dataValues, image: imageInfo },
+      });
+    } catch (err) {
+      next(err);
     }
   }
 
-  put(req: Request, res: Response, next: NextFunction) {
+  async put(req: Request, res: Response, next: NextFunction) {
     // add validation
 
-    const spiderId: number = +req.params.id;
-    const latinName = req.body.latinName || "";
-    Spider.findOne({ where: { latinName: latinName } })
-      .then((spider) => {
-        if (spider) {
+    const spiderId = +req.params.id;
+    const latinName = req.body.latinName;
+    try {
+      if (latinName) {
+        const isNameTaken = await Spider.findOne({
+          where: { latinName: latinName },
+        });
+        if (isNameTaken) {
           throw new HttpError(
             422,
             "Spider with this latin name allready exists"
           );
         }
-        return Spider.findByPk(spiderId);
-      })
-      .then((spider) => {
-        if (!spider) {
-          throw new HttpError(404, "Spider not found");
-        }
+      }
+      const spider = await Spider.findByPk(spiderId);
+      if (!spider) {
+        throw new HttpError(404, "Spider not found");
+      }
 
-        let resourcesStr: string | null;
-        if (typeof req.body.resources === "string") {
-          resourcesStr = req.body.resources;
-        } else if (req.body.resources == null) {
-          resourcesStr = null;
-        } else {
-          resourcesStr = "";
-          req.body.resources.forEach((source: string) => {
-            resourcesStr += source + " ";
-          });
-        }
+      // restore resources to string
+      let resourcesStr: string | null;
+      if (typeof req.body.resources === "string") {
+        resourcesStr = req.body.resources;
+      } else if (req.body.resources === undefined) {
+        resourcesStr = spider.resources;
+      } else if (req.body.resources == null) {
+        resourcesStr = "";
+      } else {
+        resourcesStr = "";
+        req.body.resources.forEach((source: string) => {
+          resourcesStr += source + " ";
+        });
+      }
 
-        const spiderInfo: SpiderInfo = {
-          name: req.body.name === undefined ? spider.name : req.body.name,
-          latinName: req.body.latinName || spider.latinName,
-          appearanceDesc:
-            req.body.appearanceDesc === undefined
-              ? spider.appearanceDesc
-              : req.body.appearanceDesc,
-          behaviorDesc:
-            req.body.behaviorDesc === undefined
-              ? spider.behaviorDesc
-              : req.body.behaviorDesc,
-          resources: resourcesStr === null ? spider.resources : resourcesStr,
-        };
+      Object.assign(spider, req.body, { resources: resourcesStr });
+      await spider.save();
 
-        Object.assign(spider, spiderInfo);
-        return spider.save();
-      })
-      .then((spider) => {
-        res
-          .status(200)
-          .json({ message: "Spider Info updated", spider: spider });
-      })
-      .catch((err) => {
-        next(err);
-      });
+      res.status(200).json({ message: "Spider Info updated", spider: spider });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  delete(req: Request, res: Response, next: NextFunction) {
+  async delete(req: Request, res: Response, next: NextFunction) {
     const id: number = +req.params.id;
     const includeImages: boolean = req.query.includeImages ? true : false;
 
-    Spider.findByPk(id, {
-      include: includeImages ? Spider.associations.images : undefined,
-    })
-      .then((spider) => {
-        if (!spider) {
-          throw new HttpError(404, "spider not found");
-        }
-        if (!includeImages) {
-          return spider.destroy();
-        } else {
-          spider.images?.forEach((img) => {
-            const imagePath = path.join(__dirname, "..", "public", img.src);
-            if (fs.existsSync(imagePath)) {
-              fs.unlink(imagePath, (err) => {
-                err && next(err);
-              });
-            }
-          });
-          return Image.destroy({ where: { spiderId: spider.id } }).then(() => {
-            return spider.destroy();
-          });
-        }
-      })
-      .then(() => {
-        res.status(200).json({ message: "spider deleted" });
-      })
-      .catch((err) => {
-        next(err);
+    try {
+      const spider = await Spider.findByPk(id, {
+        include: includeImages ? Spider.associations.images : undefined,
       });
+
+      if (!spider) {
+        throw new HttpError(404, "spider not found");
+      }
+
+      if (includeImages) {
+        spider.images?.forEach((img) => {
+          unlinkImg(img.src);
+        });
+        await Image.destroy({ where: { spiderId: spider.id } });
+      }
+
+      await spider.destroy();
+
+      res.status(200).json({ message: "spider deleted" });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
