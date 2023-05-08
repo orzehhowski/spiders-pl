873f44a533a5955072fcd0731c4fd5f328f51fa9
orzehhowski <filmyminecraftnayt@gmail.com>
2023-05-08 20:25:22 +0200

    docker works

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,3 +1,4 @@
+import mysql from "mysql2";
 import { Sequelize } from "sequelize-typescript";
 import Spider from "../models/spider";
 import Family from "../models/family";
@@ -10,14 +11,34 @@ interface Options {
 }
 
 const initSequelize = (options?: Options) => {
+  let db: Sequelize;
+  const dbName = options?.isTesting ? "test_spiders_pl" : "spiders_pl";
+
+  const connection = mysql.createConnection({
+    host: "db",
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
+
+  connection.query(
+    `CREATE DATABASE IF NOT EXISTS ${dbName}`,
+    function (err, results) {
+      console.log(results);
+      err && console.log(err);
+    }
+  );
+
+  connection.end();
+
   return new Sequelize({
-    database: options?.isTesting ? "test_spiders_pl" : "spiders_pl",
+    database: dbName,
     username: process.env.DB_USERNAME,
     dialect: "mysql",
-    host: "localhost",
+    host: "db",
+    port: 3306,
     password: process.env.DB_PASSWORD,
     models: [Family, Spider, Image, User, Suggestion],
-    logging: !options?.isTesting,
+    logging: options?.isTesting ? false : (sql) => console.log(sql),
   });
 };
 
