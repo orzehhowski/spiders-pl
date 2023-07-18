c5c4bda8e3201ae21828e68af908f9727a3d426b
orzehhowski <filmyminecraftnayt@gmail.com>
2023-07-18 13:03:16 +0200

    not found error site changed

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -2,6 +2,9 @@
   <main class="container">
     <h1 class="mt-3">No nieee, 404!</h1>
     <hr />
-    <h2 class="mt-2">Taka strona nie istnieje!</h2>
+    <h2 class="mt-2">
+      Taka strona nie istnieje! (lub dopiero będzie istnieć, jeśli dostałeś sie
+      tu z navbara...)
+    </h2>
   </main>
 </template>
